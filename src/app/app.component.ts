7a06cc6c9a59c4e3a90e3b5943737dabe40630df
manuel <mperjim982@iesmartinezm.es>
2022-01-26 10:55:31 +0100

    DEF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+
+  todo = '';
+  
+  tareas: any[] = []
+
+  addTodo() {
+  
+  if(this.todo != ''){
+  this.tareas = [...this.tareas, this.todo]//crea un nuevo array con lo
+  }
+  this.todo = '';
+  }
+
+  completed(todosp: number){
+    this.tareas.splice(todosp, 1)
+  }
 }
